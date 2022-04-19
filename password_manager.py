0922515b8ecbe5fc14ec88acb1310ccb461f789b
Trosin Moisei <trosin.moisei@gmail.com>
2022-04-19 20:11:14 +0200

    Init commit

---
+++ password_manager.py
@@ -0,0 +1,50 @@
+TEXT = {'steam': """""",
+'vk': """""",
+'cloud': """"""}
+
+import sys, pyperclip
+from numpy import source
+from path import Path
+import hashlib
+
+PATH = 
+key = 
+
+if len(sys.argv) < 2:
+    print('Usage: py mclip.py [keyphrase] - copy phrase text')
+    sys.exit()
+
+keyphrase = sys.argv[1] # first command line arg is the keyphrase
+
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+    print('There is no text for ' + keyphrase)
+
+import base64
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto import Random
+
+def encrypt(key, source, encode=True):
+    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    IV = Random.new().read(AES.block_size)  # generate IV
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+    padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
+    source += bytes([padding]) * padding  # Python 2.x: source += chr(padding) * padding
+    data = IV + encryptor.encrypt(source)  # store the IV at the beginning and encrypt
+    return base64.b64encode(data).decode("latin-1") if encode else data
+
+def decrypt(key, source, decode=True):
+    if decode:
+        source = base64.b64decode(source.encode("latin-1"))
+    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    IV = source[:AES.block_size]  # extract the IV from the beginning
+    decryptor = AES.new(key, AES.MODE_CBC, IV)
+    data = decryptor.decrypt(source[AES.block_size:])  # decrypt
+    padding = data[-1]  # pick the padding value from the end; Python 2.x: ord(data[-1])
+    if data[-padding:] != bytes([padding]) * padding:  # Python 2.x: chr(padding) * padding
+        raise ValueError("Invalid padding...")
+    return data[:-padding]  # remove the padding
+
